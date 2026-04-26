7c20cf1843c86b35074868efa6e20180e88819a4
Anthony-R06 <arastell@ucsc.edu>
2026-04-25 17:04:13 -0700

    first

---
+++ src/main.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// game config
+let config = {
+    parent: 'phaser-game',
+    type: Phaser.CANVAS,
+    render: {
+        pixelArt: true  // prevent pixel art from getting blurred when scaled
+    },
+    width: 600,
+    height: 700,
+    scene: [] //put scnene class name here
+}
+
+const game = new Phaser.Game(config);
